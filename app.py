1dd7bda25bb9b8323ddd68289a6331d10c411894
nitimkc <nitimkc@gmail.com>
2021-01-05 13:36:19 -0500

    percentile  calculation changed to depend on changing X_test

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
 import pickle
 import re
+import pandas as pd
 import numpy as np
 from scipy import stats
 
 # load the model
 with open(f"model/best_model.pkl", 'rb') as f:
     model = pickle.load(f) 
-with open(f"model/best_model_prob.pkl", 'rb') as f:
-    prob = pickle.load(f) 
+with open(f"model/X_test.pkl", 'rb') as f:
+    test_data = pickle.load(f) 
 with open(f"model/best_model_score.pkl", 'rb') as f:
     score = pickle.load(f) 
 with open(f"model/column_means.pkl", 'rb') as f:
@@ -45,8 +46,6 @@ def results():
         rates  = [ float(i) if float(i) < 1 else float(i)/100 for i in rates]
         tpr , tnr = rates[0], rates [1] 
         n_patient  = test_capacity if test_capacity > n_patient  else n_patient
-        print(n_patient, type(n_patient))
-        print(test_capacity, type(test_capacity))
 
         input_vals = [request.form['cough'], request.form['fever'], request.form['sorethroat'], request.form['shortnessofbreath'], 
                         request.form['headache'], request.form['sixtiesplus'], request.form['gender'], request.form['apt7'],]
@@ -80,7 +79,8 @@ def results():
                 processed_record.update({j:0.0})
 
         X = list(processed_record.values())
-
+        X_test = pd.DataFrame(test_data, columns=[k for k,v in processed_record.items()])
+        
         if sum(X[:5])==0:
             predicted_covid_prob = False
             prob_percentile = False
@@ -91,11 +91,16 @@ def results():
             policy_advice = False
         else:
             X = np.array(X).reshape(1,-1)
-            
+
+            cond1 = X_test['Ave_Pos_Past7d']>=(float(input_vals[-1])-.3)
+            cond2 = X_test['Ave_Pos_Past7d']>=(float(input_vals[-1])+.3)
+            X_test = X_test[(cond1)&(cond2)]
+
             # pass X to predict y
             y = model.predict_proba( X )[:,1]
-            y_percentile = np.round( stats.percentileofscore(prob[:, 1], y),1 )
+            y_prob = model.predict_proba(X_test)[:,1]
             
+            y_percentile = np.round( stats.percentileofscore(y_prob, y),1 )
             predicted_covid_prob = '% '.join(map(str, np.append(np.round(y*100, 1), '') ))
             prob_percentile = str(y_percentile)
             model_name = re.sub(r"(\w)([A-Z])", r"\1 \2", score['name'])
@@ -106,6 +111,9 @@ def results():
             obs_posrate = str(np.round(score['positiverate']*100,1))+'%'
             est_posrate = (score['positiverate']-1+tnr)/(tpr-1+tnr)
             est_posrate = str(np.round(est_posrate*100,1))+'%'
+            print(type(y_percentile))
+            print(type(test_capacity))
+            print(type(n_patient))
             policy_advice = np.where( y_percentile/100>1-(test_capacity/n_patient), "Test", "Do Not Test")
 
         # pass input variables and "predicted_prob" to the "render_template" function
